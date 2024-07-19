2d961a4d249a08989d2d5b0047e37e928dc2298f
khalifa-pendrops <dikee5200@gmail.com>
2024-07-19 20:43:45 +0100

    commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+
+function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
+
+  const apiKey = "393ec15437d643a3ac3164424241907";
+
+  const handleWeather = () => {
+    if (city.trim() === "") {
+      setError("Please enter a city name.");
+      setWeatherData(null);
+        return;
+    }
+
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.location) {
+          setWeatherData(data);
+          setError("");
+        } else {
+          setError("City not found. Please try again.");
+          setWeatherData(null);
+        }
+      })
+      .catch((error) => {
+        console.log("Failed to fetch weather data:", error);
+        setError("An error occurred. Please try again.");
+        setWeatherData(null);
+      });
+       setCity(" ");
+  };
+
+  return (
+    <div className="bg-gray-300  h-[100vh] flex flex-col items-center justify-center gap-6 p-8 sm:h-[50vh] ">
+      <h1 className="uppercase text-transparent  ">
+        Khalifa's Simple Weather App
+      </h1>
+      <main className="flex flex-col gap-4 justify-center items-center bg-gray-600 h-[20rem] p-6">
+        <div className="flex gap-4">
+          <p className="text-xl font-semibold">
+            Enter a city name to get weather:
+          </p>
+          <input
+            className="border-none px-[3rem] py-[.4rem] rounded-sm caret-red-500"
+            type="text"
+            id="cityInput"
+            name="city"
+            value={city}
+            placeholder="Enter city name"
+            onChange={(e) => setCity(e.target.value)}
+          ></input>
+        </div>
+        <button
+          onClick={handleWeather}
+          className="font-bold bg-blue-700 text-white px-[1rem] py-[.4rem] rounded-sm cursor-pointer"
+        >
+          Get Weather
+        </button>
+      </main>
+      <div id="weatherData">
+        {error && <p>{error}</p>}
+        {weatherData && (
+          <>
+            <h2 className="font-bold text-2xl">{weatherData.location.name}</h2>
+            <p className="italic text-sm">{weatherData.current.condition.text}</p>
+            <p className="italic text-xl">
+              Temperature:{" "}
+              <span className="text-amber-700 font-semibold">
+                {weatherData.current.temp_c}&deg;C
+              </span>
+            </p>
+            <p className="italic text-xl">
+              Humidity:{" "}
+              <span className="text-green-900 font-semibold">
+                {weatherData.current.humidity}%
+              </span>
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
